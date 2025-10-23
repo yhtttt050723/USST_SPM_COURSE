0740850fe4333232e1f11564ef463f69b5384ead
yhtttt050723 <519275808@qq.com>
2025-10-23 14:56:38 +0800

    “前后端框架搭建”

---
+++ Backend/demo/src/main/java/com/usst/spm/demo/SPMBackendApplication.java
@@ -0,0 +1,13 @@
+package com.usst.spm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SPMBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SPMBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/demo/src/test/java/com/usst/spm/demo/SPMBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usst.spm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SPMBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
